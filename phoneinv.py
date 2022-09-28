34da08c9ab32c4bcc9f1e42f7fb592dd423782a5
spa <spa.in.spa@gmail.com>
2022-09-28 08:34:27 +0300

    Version 1.0.1 Type hinting and basic code improvements

--- phoneinv.py
+++ phoneinv.py
@@ -5,21 +5,20 @@ import pandas
 import requests
 from bs4 import BeautifulSoup
 
-
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 class CiscoPhoneInventory:
-    def __init__(self):
+    def __init__(self) -> None:
         self.phone_parameters = {}
         self.phones_network = ''
 
-    def get_input_data(self):
+    def get_input_data(self) -> None:
         parser = argparse.ArgumentParser(description='Get serials and MACs of cisco phones')
         parser.add_argument('network', type=str, help='Input cisco phones network in format 192.168.0.0/24')
         self.phones_network = parser.parse_args().network
 
-    def query_network(self):
+    def query_network(self) -> None:
         for host in ip.ip_network(self.phones_network).hosts():
             host = format(host)
             try:
@@ -29,24 +28,24 @@ class CiscoPhoneInventory:
             except requests.exceptions.ConnectTimeout:
                 print(f"{host:<13}{' - ':^3}{'VOID':>4}")
 
-    def run(self):
+    def run(self) -> None:
         self.get_input_data()
         self.query_network()
         DictToExcelConverter(input_dict=self.phone_parameters).run()
 
 
 class HTTPRequester:
-    def __init__(self, phone_ip_address):
+    def __init__(self, phone_ip_address: str) -> None:
         self.phone_url = 'https://' + phone_ip_address + '/'
         self.http_tree = None
         self.mac = ''
         self.serial = ''
 
-    def get_http_tree(self):
+    def get_http_tree(self) -> None:
         http_response = requests.get(self.phone_url, verify=False, timeout=2)
         self.http_tree = BeautifulSoup(http_response.content, 'lxml')
 
-    def extract_phone_parameters(self):
+    def extract_phone_parameters(self) -> None:
         phone_parameters_table = self.http_tree.findAll('table')[2]
         for child in phone_parameters_table.children:
             parameter_name = child.findAll('td')[0].string.lstrip()
@@ -56,23 +55,23 @@ class HTTPRequester:
             elif parameter_name.startswith('Serial'):
                 self.serial = parameter_value
 
-    def run(self):
+    def run(self) -> (str, str):
         self.get_http_tree()
         self.extract_phone_parameters()
         return self.mac, self.serial
 
 
 class DictToExcelConverter:
-    def __init__(self, input_dict, output_excel='serials_and_macs.xlsx'):
+    def __init__(self, input_dict: dict, output_excel='serials_and_macs.xlsx'):
         self.input_dict = input_dict
         self.output_excel = os.path.join(BASE_DIR, output_excel)
 
-    def write_excel(self):
+    def write_excel(self) -> None:
         _frame = pandas.DataFrame.from_dict(self.input_dict, orient='index', columns=['Serial'])
         _frame.index.name = 'MAC'
         _frame.to_excel(self.output_excel)
 
-    def run(self):
+    def run(self) -> None:
         self.write_excel()
 
 
